b745c9d8dd0ebe41a6166d267d09423102ccd98c
A.M <andares@outlook.com>
2018-06-29 10:49:25 +0800

    update

--- src/luc/functions.php
+++ src/luc/functions.php
@@ -4,8 +4,13 @@ namespace luc;
 
 use Luclin\Module;
 
+use App;
 use Illuminate\Support\Arr;
 use Illuminate\Filesystem\Filesystem;
+
+function env($change = null): string {
+    return App::environment($change);
+}
 
 function mod(string $name, string $prefix = 'lumod:'): Module {
     return app("$prefix$name");
