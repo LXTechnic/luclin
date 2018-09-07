42c5cd565787c7aea6ff118cb6ec170e31802bec
A.M <andares@outlook.com>
2018-09-07 12:54:59 +0800

    update

--- src/Protocol/Response.php
+++ src/Protocol/Response.php
@@ -16,19 +16,24 @@ class Response extends Container
 
     public function created(?string $resource = null, $headers = []) {
         $resource && $headers['Location'] = $resource;
+        $this->setContract('code', 201);
         return $this->send(201, $headers);
     }
 
     public function wait(?string $operation = null, $headers = []) {
         $operation && $headers['Operation-Location'] = $operation;
+        $this->setContract('code', 202);
         return $this->send(202, $headers);
     }
 
     public function iHaveNotIdea($headers = []) {
+        $this->setContract('code', 203);
         return $this->send(203, $headers);
     }
 
     public function ok(bool $refresh = false, $headers = []) {
-        return $this->send($refresh ? 205 : 204, $headers);
+        $code = $refresh ? 205 : 204;
+        $this->setContract('code', $code);
+        return $this->send($code, $headers);
     }
 }
