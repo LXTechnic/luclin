375c3b5e587965bdbc48c243884c78359fc3a54c
A.M <andares@outlook.com>
2018-10-15 15:09:01 +0800

    update

---
+++ src/Cabin/Model/Extra.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Luclin\Cabin\Model;
+
+use Luclin\Cabin\Foundation\Model;
+
+use Illuminate\Database\Schema\Blueprint;
+
+abstract class Extra extends Model
+{
+    use Traits\Query;
+
+    public function __construct(array $attributes = []) {
+        $id = $attributes[$this->primaryKey] ?? $attributes["_$this->primaryKey"] ?? null;
+
+        parent::__construct($attributes);
+    }
+
+    public static function makeWith($master): self {
+        $model = new static();
+        if (is_object($master)) {
+            $model->setId($master->id());
+        } elseif (is_array($master)) {
+            $model->setId($master[$model->primaryKey] ?? $master["_$model->primaryKey"] ?? null);
+        } else {
+            $model->setId($master);
+        }
+
+        return $model;
+    }
+
+    protected static function migrateUpPrimary(Blueprint $table,
+        bool $numeric = true): void
+    {
+        $numeric ? $table->bigInteger('id') : $table->string('id', 100);
+        $table->primary('id');
+    }
+
+}
