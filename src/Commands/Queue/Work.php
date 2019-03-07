cba9e2667062a4a103cd9ae164a9a0f1c5e313fd
A.M <andares@outlook.com>
2019-03-07 15:39:02 +0800

    add luc:queue:work command

---
+++ src/Commands/Queue/Work.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Luclin\Commands\Queue;
+
+use Illuminate\Queue\Console;
+
+class Work extends Console\WorkCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'luc:queue:work
+                            {connection? : The name of the queue connection to work}
+                            {--queue= : The names of the queues to work}
+                            {--daemon : Run the worker in daemon mode (Deprecated)}
+                            {--once : Only process the next job on the queue}
+                            {--stop-when-empty : Stop when the queue is empty}
+                            {--delay=0 : The number of seconds to delay failed jobs}
+                            {--force : Force the worker to run even in maintenance mode}
+                            {--memory=128 : The memory limit in megabytes}
+                            {--sleep=3 : Number of seconds to sleep when no job is available}
+                            {--timeout=60 : The number of seconds a child process can run}
+                            {--tries=0 : Number of times to attempt a job before logging it failed}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '执行监听队列';
+
+    /**
+     * The queue worker instance.
+     *
+     * @var \Illuminate\Queue\Worker
+     */
+    protected $worker;
+
+    /**
+     * Get the queue name for the worker.
+     *
+     * @param  string  $connection
+     * @return string
+     */
+    protected function getQueue($connection)
+    {
+        return $connection;
+    }
+
+}
